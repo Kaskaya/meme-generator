3f7d930b839b5acf421191c251e968ee19258d6d
Kaskaya <recep.tkaskaya@gmail.com>
2023-09-04 15:18:27 +0300

    asdasd

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,16 @@
+import { useState, useEffect } from "react";
+import Api from "./Api";
+
 export default function Meme() {
+  const [state, setState] = useState("https://i.imgflip.com/30b1gx.jpg");
+
+  function changeMeme() {
+    const randomNumber = Math.floor(Math.random() * Api.length);
+    setState(Api[randomNumber].url);
+  }
+
   return (
-    <main className=" grid justify-center">
+    <main className=" grid justify-center items-center">
       <div className="flex justify-center m-12  ">
         <input
           type="text"
@@ -13,10 +23,15 @@ export default function Meme() {
           className="w-72 h-14 border-2 border-slate-300 rounded-xl mx-4 px-3 outline-none text-xl"
         />
       </div>
-      <button className="bg-gradient-to-r from-custom-main to-custom-secondary px-32 py-4 text-white text-3xl rounded-3xl">
+      <button
+        onClick={changeMeme}
+        className="bg-gradient-to-r from-custom-main to-custom-secondary px-32 py-4 text-white text-3xl rounded-3xl"
+      >
         Get A New Meme
       </button>
-      <h1>Meme</h1>
+      <div className="w-96">
+        <img src={state} className=" h-full" />
+      </div>
     </main>
   );
 }
--- src/Memes.js
+++ src/Memes.js
@@ -1,10 +1,23 @@
+import { useState, useEffect } from "react";
+/* const URL = "https://api.imgflip.com/caption_image"; */
+
 const Memes = function () {
-  fetch("https://api.imgflip.com/get_memes")
-    .then((res) => res.json())
-    .then((data) => {
+  const [resourceType, setResourceType] = useState("get_memes");
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    fetch(`https://api.imgflip.com/${resourceType}`)
+      .then((res) => res.json())
+      .then((json) => {
+        setItems(json.data.memes);
+        console.log(json.data.memes);
+      });
+
+    /* return () => {
       console.log(data.data.memes);
-      return data.data.memes;
-    });
+    }; */
+  }, [resourceType]);
+
+  return <div></div>;
 };
 
 export default Memes;
